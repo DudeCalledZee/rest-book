d7d6aa2d024282e96e0e3ee448dde3d40fbb3a3d
Zee <ineedtothink@gmail.com>
2024-10-14 15:31:23 +0100

    creating data class and expanding on controller

---
+++ src/main/kotlin/org/project/Book.kt
@@ -0,0 +1,9 @@
+package org.project
+
+data class Book(
+    val id: Int,
+    val title: String,
+    val author: String,
+    val genre: String,
+    val yearOfPublication: Int
+)
--- src/main/kotlin/org/project/BookResource.kt
+++ src/main/kotlin/org/project/BookResource.kt
@@ -2,13 +2,56 @@ package org.project
 
 import jakarta.ws.rs.GET
 import jakarta.ws.rs.Path
+import jakarta.ws.rs.PathParam
 import jakarta.ws.rs.Produces
 import jakarta.ws.rs.core.MediaType
+import java.util.*
 
 @Path("api/books")
+@Produces(MediaType.APPLICATION_JSON)
 class BookResource {
 
     @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    fun books() =
+        listOf(
+            Book(
+                id = 1,
+                title = "The Hobbit",
+                author = "<NAME>",
+                genre = "Fantasy",
+                yearOfPublication = 1937
+            ),
+            Book(
+                id = 2,
+                title = "The Lord of the Rings",
+                author = "<NAME>",
+                genre = "Fantasy",
+                yearOfPublication = 1954
+            ),
+            Book(
+                id = 3,
+                title = "The Hitchhiker's Guide to the Galaxy",
+                author = "<NAME>",
+                genre = "Science Fiction",
+                yearOfPublication = 1979
+            ),
+            Book(
+                id = 4,
+                title = "The Adventures of Huckleberry Finn",
+                author = "<NAME>",
+                genre = "Children's Books",
+                yearOfPublication = 1988
+            ),
+        )
+
+    @GET
+    @Path("/count")
     @Produces(MediaType.TEXT_PLAIN)
-    fun hello() = "Hello Just another day another application"
+    fun countAllBooks() = books().size.toString()
+
+    @GET
+    @Path("{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    fun getBookById(@PathParam("id") id: Int) = books().find { it.id == id }?.let { Optional.of(it) }
 }
